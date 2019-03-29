6383f1d35be54602c56505f208154cde900d6dfa
Erin Grooms <eringrooms3746@gmail.com>
2019-03-29 15:15:30 -0500

    edited MesoEqual.java (not complete yet)

--- MesoEqual.java
+++ MesoEqual.java
@@ -1,4 +1,16 @@
+import java.util.HashMap;
 
 public class MesoEqual {
+	
+	HashMap<String, Integer> stid = new HashMap<String, Integer>();
+	
+	public void calAsciiEqual() {
+		for(String stID : stid.keySet()) {
+			
+			if(stid.get(stID).equals(stid.values())){
+				
+			}
+		}
+	}
 
 }
