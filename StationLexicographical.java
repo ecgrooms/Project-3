fbda0482f8bca7bdb09748f22ad54d438d3a2b56
Erin Grooms <eringrooms3746@gmail.com>
2019-03-30 21:04:23 -0500

    edited

--- StationLexicographical.java
+++ StationLexicographical.java
@@ -4,8 +4,10 @@ import java.util.TreeMap;
 
 public class StationLexicographical extends MesoSortedAbstract{
 	
+	//A map that sorted the list of station IDs and their values
 	TreeMap<String, Integer> sMap = new TreeMap<>();
 	
+	//sort the list and return the whole lists of station IDs and values
 	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
 		
 		sMap.putAll(unsorted);
